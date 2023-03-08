6d67a3aa44e70425b9e5ed07f8a5686e3c0d79ea
truong giang <nguyentruonggiang99hb@gmail.com>
2023-03-09 00:19:09 +0700

    up

--- routes/product.api.js
+++ routes/product.api.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const { getAllProduct } = require("../controller/product.controller");
-const authentication = require("../middlwe/authentication");
 const validations = require("../middlwe/validations");
 const router = express.Router();
 
 // get all product
-router.post("/allproduct", validations.validate([]), getAllProduct);
+router.get("/allproduct", validations.validate([]), getAllProduct);
 module.exports = router;
